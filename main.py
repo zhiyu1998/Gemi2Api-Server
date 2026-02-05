e35866ea7a178928269da5bd5a5298c730c43c65
Yang ShouYuan <shouyuan.yang@timesoftsg.com.sg>
2026-02-05 18:56:50 +0800

    feat: Add new `sec-ch-ua` and `sec-fetch-storage-access` headers to the request configuration.

--- main.py
+++ main.py
@@ -237,12 +237,22 @@ async def proxy_image(url: str, sig: str):
 		"pragma": "no-cache",
 		"priority": "u=1, i",
 		"sec-ch-ua": '"Not(A:Brand";v="8", "Chromium";v="144", "Microsoft Edge";v="144"',
+		"sec-ch-ua-arch": '"x86"',
+		"sec-ch-ua-bitness": '"64"',
+		"sec-ch-ua-form-factors": '"Desktop"',
+		"sec-ch-ua-full-version": '"144.0.3719.104"',
+		"sec-ch-ua-full-version-list": '"Not(A:Brand";v="8.0.0.0", "Chromium";v="144.0.7559.110", "Microsoft Edge";v="144.0.3719.104"',
 		"sec-ch-ua-mobile": "?0",
+		"sec-ch-ua-model": '""',
 		"sec-ch-ua-platform": '"Windows"',
+		"sec-ch-ua-platform-version": '"19.0.0"',
+		"sec-ch-ua-wow64": "?0",
 		"sec-fetch-dest": "image",
 		"sec-fetch-mode": "no-cors",
 		"sec-fetch-site": "cross-site",
+		"sec-fetch-storage-access": "none",
 	}
+
 
 
 	# IMPORTANT: Use a clean AsyncClient WITHOUT the Gemini session cookies.
